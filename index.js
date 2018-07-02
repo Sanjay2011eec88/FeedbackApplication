0980b63f1f799488cf9c489fedc51a961f8b72df
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-07-02 18:10:40 +0530

    Comment error

--- index.js
+++ index.js
@@ -7,17 +7,7 @@ app.get('/', (req, res) => {
     res.send({ hi: 'there'});
 });
 
-/*
-* To run in heroku we need to set couple of things:
-* 1.Set port to env variable if it their otherwise use the predefined port.
-* 2.Specify node environment i.e. we need to specify the node version we are going to use
-* This can be done by adding following in the package.json file
-* "engine":{
-* "node":"8.1.1",
-* "npm":"5.0.3"
-* }*
-* 3.Specify a start script
-* /
+
 
 //We can set the port by using env varibales or it will be 5000
 const PORT  = process.env.PORT || 5000;
